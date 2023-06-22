c68867464073c29851268306f4a3c5cffac94f28
Ashwiga <130762038+Ashwiga@users.noreply.github.com>
2023-06-22 19:38:45 +0530

    Create l4.py

---
+++ l4.py
@@ -0,0 +1,15 @@
+n=int(input("enter the elements in the list="))
+num=[]
+flag=0
+i=1
+while i<len(num):
+    if(num[i]-num[i])
+    flag=1
+i=i+1
+if(not flag):
+    print("ascending order")
+else:
+    print("not in ascending order")
+
+
+
